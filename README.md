1ab43427579badbf5700f9d95fc1e4a114dbe9c7
Waseem Ahmad <talk.to.waseem@gmail.com>
2017-03-21 01:23:47 +0530

    Print more all fields.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-xkcd
 xkcd downloads and prints json of an xkcd comic given its number
+
+`$ go run comic.go 55.json`
--- comic.go
+++ comic.go
@@ -106,4 +106,10 @@ func main() {
 	fmt.Printf("+----------+\n\n")
 	fmt.Printf("Title: %s\n", comic.Title)
 	fmt.Printf("Safe Title: %s\n", comic.SafeTitle)
+	fmt.Printf("Transcript: %s\n", comic.Transcript)
+	fmt.Printf("Alt: %s\n", comic.Alt)
+	fmt.Printf("Image: %s\n", comic.Img)
+	fmt.Printf("Link: %s\n", comic.Link)
+	fmt.Printf("Date: %s-%s-%s\n", comic.Year, comic.Month, comic.Day)
+	fmt.Printf("News: %s\n", comic.News)
 }
